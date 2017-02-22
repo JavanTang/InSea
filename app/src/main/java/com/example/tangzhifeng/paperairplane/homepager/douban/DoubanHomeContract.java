7bf423e1b9ff5132268c1caeccd9b4d8f86b7a51
唐志峰 <tzfjobmail@gmail.com>
2017-02-22 18:41:35 +0800

    修改了APP名字与创建了豆瓣的包

---
+++ app/src/main/java/com/example/tangzhifeng/paperairplane/homepager/douban/DoubanHomeContract.java
@@ -0,0 +1,10 @@
+package com.example.tangzhifeng.paperairplane.homepager.douban;
+
+/**
+ * 作者: tangzhifeng on 2017/2/21.
+ * 邮箱: tzfjobmail@gmail.com
+ */
+
+public interface DoubanHomeContract {
+
+}
---
+++ app/src/main/java/com/example/tangzhifeng/paperairplane/homepager/douban/DoubanHomePrsenter.java
@@ -0,0 +1,9 @@
+package com.example.tangzhifeng.paperairplane.homepager.douban;
+
+/**
+ * 作者: tangzhifeng on 2017/2/21.
+ * 邮箱: tzfjobmail@gmail.com
+ */
+
+public class DoubanHomePrsenter {
+}
